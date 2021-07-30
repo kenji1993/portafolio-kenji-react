bc687471f3c74ea525556e7cdc93ce3995248e5e
Kenji <kenji.arakaki93@gmail.com>
2021-07-30 20:35:25 -0300

    Constant scope fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './Footer.css'
+import { images } from '../../helpers/constants'
 
 const Footer = () => {
+
+    const { githubFooter, linkedinFooter } = images.contact
+
     return (
         <div className="footerContainer">
             <div className="container">
-                <h1>Footer</h1> 
+                <a className="footerContact" href={linkedinFooter.url}>
+                    <img src={linkedinFooter.img} alt="Linkedin" /> 
+                </a>
+                <a className="footerContact" href={githubFooter.url}>
+                    <img src={githubFooter.img} alt="Github" /> 
+                </a>
             </div>
         </div>
     )
--- src/helpers/constants.jsx
+++ src/helpers/constants.jsx
@@ -1,3 +1,5 @@
+// Iconos //
+
 import html from '../assets/images/html-icon.png'
 import css from '../assets/images/css3-icon.png'
 import bootstrap from '../assets/images/bootstrap-icon.png'
@@ -7,7 +9,11 @@ import typescript from '../assets/images/typescript-icon.png'
 import github from '../assets/images/github-icon.png'
 import githubFooter from '../assets/images/github-footer-icon.png'
 import linkedin from '../assets/images/linkedin-icon.png'
+import linkedinFooter from '../assets/images/linkedin-footer-icon.png'
 import gmail from '../assets/images/gmail-icon.png'
+
+// Proyectos //
+
 import atom from '../assets/images/atom.jpg'
 import calculadora from '../assets/images/calculadora.jpeg'
 import cassette from '../assets/images/cassette.jpg'
@@ -19,39 +25,10 @@ import kenCoin from '../assets/images/kenCoin.jpeg'
 import randomPage from '../assets/images/randomPage.jpg'
 import spaceInvader from '../assets/images/spaceInvader.jpg'
 
+// Experiencia //
+import logoEmergencias from '../assets/images/logoEmergencias.png'
+import logoBoreal from '../assets/images/logoBoreal.svg'
 
-export const images =  Object.freeze({
-    knowledge: {
-        html,
-        css,
-        bootstrap,
-        javascript,
-        react,
-        typescript
-    },
-    contact: {
-        github,
-        githubFooter,
-        linkedin,
-        gmail
-    },
-    cssGalery: {
-        atom,
-        cassette,
-        cubeMove,
-        deadmau5,
-        spaceInvader
-    },
-    projects: {
-        randomPage,
-        kenjinux,
-        pronunciation
-    },
-    reactNative: {
-        calculadora,
-        kenCoin
-    }
-})
 
 export const urls = Object.freeze({
     linkedinUrl: 'https://www.linkedin.com/in/kenji-arakaki/',
@@ -89,6 +66,58 @@ export const urls = Object.freeze({
         },
         cassette: 'Cassette',
         github: 'https://github.com/kenji1993/cassetteCSS'
+    }
+})
+
+
+
+export const images =  Object.freeze({
+    knowledge: {
+        html,
+        css,
+        bootstrap,
+        javascript,
+        react,
+        typescript
+    },
+    contact: {
+        github: {
+            url: urls.githubUrl,
+            img: github
+        },
+        githubFooter: {
+            url: urls.githubUrl,
+            img: githubFooter,
+        },
+        linkedin: {
+            url: urls.linkedinUrl,
+            img: linkedin
+        },
+        linkedinFooter: {
+            url: urls.linkedinUrl,
+            img: linkedinFooter
+        },
+        gmail
+    },
+    cssGalery: {
+        atom,
+        cassette,
+        cubeMove,
+        deadmau5,
+        spaceInvader
+    },
+    projects: {
+        randomPage,
+        kenjinux,
+        pronunciation
+    },
+    reactNative: {
+        calculadora,
+        kenCoin
+    },
+    experience: {
+        logoBoreal,
+        logoEmergencias,
     }
 })
 
